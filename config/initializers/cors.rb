2540bb2be21c8b4dbca6f0ffa92a257bffc49943
Zijian Zhou <zephyroszhou@gmail.com>
2020-01-24 10:55:48 +0800

    update cors files

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,18 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://cvwo-website-zijian.herokuapp.com/"
+        origins "http://localhost:3000"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+credentials: true
+    end
+    
+    allow do
+        origins "http://cvwo-website-zijian.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
 credentials: true
     end
 
     allow do
-        origins "http://cvwo-zijian.herokuapp.com/"
+        origins "http://cvwo-zijian.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
 credentials: true
     end
