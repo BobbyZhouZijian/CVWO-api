0862769f81f163a6e519849f3515ebe9f3492d6c
Zijian Zhou <zephyroszhou@gmail.com>
2020-01-24 12:53:34 +0800

    fix bug regarding session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "test_app", domain: "cvwo-zijian.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "test_app", domain: "cvwo-zijian.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "test_app"
 end
